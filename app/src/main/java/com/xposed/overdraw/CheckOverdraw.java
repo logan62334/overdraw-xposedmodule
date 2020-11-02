00a756e4399f3273fef85c0d4a4999705aa6056f
mafei <mafei@xinbao-deMacBook-Pro.local>
2020-11-02 20:59:07 +0800

    init

---
+++ app/src/main/java/com/xposed/overdraw/CheckOverdraw.java
@@ -0,0 +1,155 @@
+package com.xposed.overdraw;
+
+import android.annotation.SuppressLint;
+import android.os.Environment;
+import android.text.TextUtils;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+
+
+public class CheckOverdraw implements IXposedHookLoadPackage {
+
+    private float overDrawCounter;
+
+    @SuppressLint("PrivateApi")
+    @Override
+    public void handleLoadPackage(LoadPackageParam loadPackageParam) {
+
+        try {
+            Class<?> hookMessageListenerClass;
+            hookMessageListenerClass = loadPackageParam.classLoader.loadClass("android.view.HardwareCanvas");
+            findAndHookMethod("android.view.HardwareRenderer$GlRenderer", loadPackageParam.classLoader, "drawOverdrawCounter", hookMessageListenerClass, float.class, float.class, new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) {
+                    overDrawCounter = (float) param.args[1];
+                    writeFileToSDCard(float2byte(overDrawCounter), null, "overDraw.txt", false, false);
+                }
+            });
+        } catch (Throwable e) {
+            Log.d("overdraw", "falied:" + e.toString());
+        }
+
+    }
+
+    public static byte[] float2byte(float f) {
+
+        // 把float转换为byte[]
+        int fbit = Float.floatToIntBits(f);
+
+        byte[] b = new byte[4];
+        for (int i = 0; i < 4; i++) {
+            b[i] = (byte) (fbit >> (24 - i * 8));
+        }
+
+        // 翻转数组
+        int len = b.length;
+        // 建立一个与源数组元素类型相同的数组
+        byte[] dest = new byte[len];
+        // 为了防止修改源数组，将源数组拷贝一份副本
+        System.arraycopy(b, 0, dest, 0, len);
+        byte temp;
+        // 将顺位第i个与倒数第i个交换
+        for (int i = 0; i < len / 2; ++i) {
+            temp = dest[i];
+            dest[i] = dest[len - i - 1];
+            dest[len - i - 1] = temp;
+        }
+
+        return dest;
+
+    }
+
+
+    /**
+     * 此方法为android程序写入sd文件文件，用到了android-annotation的支持库@
+     *
+     * @param buffer   写入文件的内容
+     * @param folder   保存文件的文件夹名称,如log；可为null，默认保存在sd卡根目录
+     * @param fileName 文件名称，默认overDraw.txt
+     * @param append   是否追加写入，true为追加写入，false为重写文件
+     * @param autoLine 针对追加模式，true为增加时换行，false为增加时不换行
+     */
+    public synchronized static void writeFileToSDCard(@NonNull final byte[] buffer, @Nullable final String folder,
+                                                      @Nullable final String fileName, final boolean append, final boolean autoLine) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                boolean sdCardExist = Environment.getExternalStorageState().equals(
+                        android.os.Environment.MEDIA_MOUNTED);
+                String folderPath = "";
+                if (sdCardExist) {
+                    //TextUtils为android自带的帮助类
+                    if (TextUtils.isEmpty(folder)) {
+                        //如果folder为空，则直接保存在sd卡的根目录
+                        folderPath = Environment.getExternalStorageDirectory()
+                                + File.separator;
+                    } else {
+                        folderPath = Environment.getExternalStorageDirectory()
+                                + File.separator + folder + File.separator;
+                    }
+                } else {
+                    return;
+                }
+
+                File fileDir = new File(folderPath);
+                if (!fileDir.exists()) {
+                    if (!fileDir.mkdirs()) {
+                        return;
+                    }
+                }
+                File file;
+                //判断文件名是否为空
+                if (TextUtils.isEmpty(fileName)) {
+                    file = new File(folderPath + "overDraw.txt");
+                } else {
+                    file = new File(folderPath + fileName);
+                }
+                RandomAccessFile raf = null;
+                FileOutputStream out = null;
+                try {
+                    if (append) {
+                        //如果为追加则在原来的基础上继续写文件
+                        raf = new RandomAccessFile(file, "rw");
+                        raf.seek(file.length());
+                        raf.write(buffer);
+                        if (autoLine) {
+                            raf.write("\n".getBytes());
+                        }
+                    } else {
+                        //重写文件，覆盖掉原来的数据
+                        out = new FileOutputStream(file);
+                        out.write(buffer);
+                        out.flush();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        if (raf != null) {
+                            raf.close();
+                        }
+                        if (out != null) {
+                            out.close();
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
+
+}
